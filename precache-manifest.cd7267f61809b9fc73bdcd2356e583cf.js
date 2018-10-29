420373d6dc5605360b746a39f0f208d7ec10af00
Mortemos <34795338+Mortemos@users.noreply.github.com>
2018-10-29 16:43:17 +0200

    Updates

---
+++ precache-manifest.cd7267f61809b9fc73bdcd2356e583cf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a792770b6ea4478cc049",
+    "url": "/test-task/static/js/runtime~main.a792770b.js"
+  },
+  {
+    "revision": "79be2c2ceab56cee2ce3",
+    "url": "/test-task/static/js/main.79be2c2c.chunk.js"
+  },
+  {
+    "revision": "7422ab352141db5d328a",
+    "url": "/test-task/static/js/1.7422ab35.chunk.js"
+  },
+  {
+    "revision": "79be2c2ceab56cee2ce3",
+    "url": "/test-task/static/css/main.4c09df39.chunk.css"
+  },
+  {
+    "revision": "e0dff1dcf4fe97f3c9889661985d76e9",
+    "url": "/test-task/index.html"
+  }
+];
